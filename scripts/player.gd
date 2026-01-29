dc185ec642ac23c353025eb89a8d5e58f8cc6797
Grayson Riffe <graysonriffe@yahoo.com>
2026-01-28 20:49:27 -0600

    Add head bobbing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,21 @@
-# Player script - handles FPS movement basic gameplay
+# Player script - handles FPS movement and basic gameplay
 class_name Player
 extends CharacterBody3D
 
-
+# Constants
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 0.3
 
+# FPS controller variables
 var movementDirectionSmoothed: Vector3
+var headBobbingVector: Vector2
+var headBobbingTheta: float
 
-@onready var head: MeshInstance3D = $Head
+@onready var head: Node3D = $Head
+@onready var eyes: Node3D = $Head/Eyes
 
+# All we need to do here for now is capture the mouse
 func _ready() -> void:
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -28,29 +33,41 @@ func _physics_process(delta: float) -> void:
     var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
     var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
     
+    # Smooth the movement, and differently depending on if the player is mid-air
     if is_on_floor():
         movementDirectionSmoothed = lerp(movementDirectionSmoothed, direction, 10.0 * delta)
     elif input_dir != Vector2.ZERO:
         movementDirectionSmoothed = lerp(movementDirectionSmoothed, direction, 3.0 * delta)
     
-    if movementDirectionSmoothed:
-        velocity.x = movementDirectionSmoothed.x * SPEED
-        velocity.z = movementDirectionSmoothed.z * SPEED
+    # Do headbobbing when walking, and reset when not
+    if direction != Vector3.ZERO:
+        headBobbingTheta += 14.0 * delta
+        headBobbingVector = Vector2(sin(headBobbingTheta / 2) + 0.5, sin(headBobbingTheta))
+        eyes.position.x = lerp(eyes.position.x, headBobbingVector.x * 0.1, 10.0 * delta)
+        eyes.position.y = lerp(eyes.position.y, headBobbingVector.y * 0.05, 10.0 * delta)
     else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
-        velocity.z = move_toward(velocity.z, 0, SPEED)
+        eyes.position.x = lerp(eyes.position.x, 0.0, 10.0 * delta)
+        eyes.position.y = lerp(eyes.position.y, 0.0, 10.0 * delta)
+        headBobbingTheta = 0.0
+    
+    # Apply the movement
+    velocity.x = movementDirectionSmoothed.x * SPEED
+    velocity.z = movementDirectionSmoothed.z * SPEED
 
     move_and_slide()
 
 
+# Handle all other inputs
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_released("pause"):
         Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
     
+    # Click -> capture mouse
     if event is InputEventMouseButton:
         if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
             Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
     
+    # When mouse is captured, mouse movement -> FPS head movement
     if event is InputEventMouseMotion:
         if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
             rotate_y(-deg_to_rad(event.relative.x * MOUSE_SENSITIVITY))
