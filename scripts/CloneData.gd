4e5b35eda09e65a5f1cf0e1dfd0741639cd84947
Grayson Riffe <graysonriffe@yahoo.com>
2026-02-02 15:48:17 -0600

    Add basic player input recording and playback
    R starts and stops recording
    F starts and stops time when not recording

---
+++ scripts/CloneData.gd
@@ -0,0 +1,49 @@
+# CloneData - A resource that contains everything a clone needs to replicate player actions
+class_name CloneData
+extends Resource
+
+## A vector of Input.getVector Vector2s from player inputs on physics frames
+var recordedMovement: Array[Vector2]
+
+## A vector of pitch and yaw pairs of the player's camera view on physics frames
+var recordedLookVectors: Array[Vector2]
+
+## A vector of the state of the jump button on physics frames
+var recordedJumpButton: Array[bool]
+
+func getRecordSize() -> int:
+    return recordedMovement.size()
+
+
+func getNextTimeIndex(prevIndex: int) -> int:
+    return 0 if prevIndex + 1 >= getRecordSize() else prevIndex + 1
+
+
+func clear():
+    recordedMovement.clear()
+    recordedLookVectors.clear()
+    recordedJumpButton.clear()
+
+
+func pushBackMovementVector(vec: Vector2):
+    recordedMovement.push_back(vec)
+
+
+func getMovementVector(index: int) -> Vector2:
+    return recordedMovement[index]
+
+
+func pushBackLookVector(vec: Vector2):
+    recordedLookVectors.push_back(vec)
+
+
+func getLookVector(index: int) -> Vector2:
+    return recordedLookVectors[index]
+
+
+func pushBackJump(jump: bool):
+    recordedJumpButton.push_back(jump)
+
+
+func getJumpButton(index: int) -> bool:
+    return recordedJumpButton[index]
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -12,26 +12,69 @@ var movementDirectionSmoothed: Vector3
 var headBobbingVector: Vector2
 var headBobbingTheta: float
 
+# Time variables
+var timePassing: bool
+var timeIndex: int
+
+# Recording variables
+var recordingCurrently: bool
+var recordingCloneData: CloneData
+
 @onready var head: Node3D = $Head
 @onready var eyes: Node3D = $Head/Eyes
+
+@onready var statusLabel: Label = get_tree().root.find_child("StatusLabel", true, false)
 
 # All we need to do here for now is capture the mouse
 func _ready() -> void:
     Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    
+    timePassing = false
+    timeIndex = 0
+    recordingCurrently = false
+    recordingCloneData = CloneData.new()
+    
+    statusLabel.text = "Status: Time Stopped"
 
 
 func _physics_process(delta: float) -> void:
+    # Get the input direction and handle the movement/deceleration.
+    var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+    
+    # Record input_dir if recording is enabled
+    if recordingCurrently:
+        recordingCloneData.pushBackMovementVector(input_dir)
+        var currentLookVector = Vector2(head.global_rotation.x, global_rotation.y)
+        recordingCloneData.pushBackLookVector(currentLookVector)
+        recordingCloneData.pushBackJump(Input.is_action_pressed("jump"))
+    
+    var direction: Vector3
+    
+    if timePassing:
+        if timeIndex == 0:
+            position = Vector3.ZERO
+            rotation = Vector3.ZERO
+        
+        # Set the movement
+        var nextMovement: Vector2 = recordingCloneData.getMovementVector(timeIndex)
+        timeIndex = recordingCloneData.getNextTimeIndex(timeIndex)
+        direction = (transform.basis * Vector3(nextMovement.x, 0, nextMovement.y)).normalized()
+        
+        # Set the look vector
+        var nextLookVector: Vector2 = recordingCloneData.getLookVector(timeIndex)
+        global_rotation.y = nextLookVector.y
+        head.global_rotation.x = nextLookVector.x
+    else:
+        direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+    
     # Add the gravity.
     if not is_on_floor():
         velocity += get_gravity() * delta
 
     # Handle jump.
-    if Input.is_action_just_pressed("jump") and is_on_floor():
+    if (Input.is_action_just_pressed("jump") and is_on_floor() and not timePassing)\
+        or (timePassing and recordingCloneData.getJumpButton(timeIndex)):
         velocity.y = JUMP_VELOCITY
-
-    # Get the input direction and handle the movement/deceleration.
-    var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-    var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
     
     # Smooth the movement, and differently depending on if the player is mid-air
     if is_on_floor():
@@ -68,8 +111,24 @@ func _unhandled_input(event: InputEvent) -> void:
             Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
     
     # When mouse is captured, mouse movement -> FPS head movement
-    if event is InputEventMouseMotion:
+    if event is InputEventMouseMotion and not timePassing:
         if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
             rotate_y(-deg_to_rad(event.relative.x * MOUSE_SENSITIVITY))
             head.rotate_x(-deg_to_rad(event.relative.y * MOUSE_SENSITIVITY))
             head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+    
+    if event.is_action_released("record_clone") and not timePassing:
+        if recordingCurrently == false:
+            recordingCloneData.clear()
+        
+        recordingCurrently = not recordingCurrently
+        
+        statusLabel.text = "Status: Recording" if recordingCurrently else "Status: Time Stopped"
+    
+    if event.is_action_released("time_play_and_stop") and not recordingCurrently:
+        if timePassing == false:
+            timeIndex = 0
+        
+        timePassing = not timePassing
+        
+        statusLabel.text = "Status: Time Passing" if timePassing else "Status: Time Stopped"
