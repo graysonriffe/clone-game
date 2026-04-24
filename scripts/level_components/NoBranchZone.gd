40e1010245db0b5d594a2817a719ba7839cb89bc
Grayson Riffe <graysonriffe@yahoo.com>
2026-04-24 01:42:32 -0500

    NoBranchZone particles now have consistent density

--- scripts/level_components/NoBranchZone.gd
+++ scripts/level_components/NoBranchZone.gd
@@ -3,3 +3,9 @@ class_name NoBranchZone
 extends Area3D
 
 const CLASS_NAME = "NoBranchZone"
+
+@onready var particles: GPUParticles3D = $GPUParticles3D
+
+func _ready() -> void:
+    var volume: float = scale.x * scale.y * scale.z
+    particles.amount = (volume * 0.5) as int
